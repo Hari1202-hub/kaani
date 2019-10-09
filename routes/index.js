9de640265a1abca0d3905789f397548b29adcc62
Hari <mhariprasanth769@gmail.com>
2019-10-09 18:04:38 +0530

    app.js changed
    app.js changed

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET home page. */
-router.get("/", function(request, response) {
-  response.render("index.html");
-});
-
-module.exports = router;
